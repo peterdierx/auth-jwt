29eca2631a88689a9f8c657157d87707f418109b
Peter Dierx <peterdierx@gmail.com>
2019-08-18 16:36:39 +0200

    bulma css and home component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+// MODULES
 import Vue    from 'vue'
 import App    from '@/App'
 import router from '@/router'
 
+// BULMA
+import 'bulma/css/bulma.css'
+
+// CONFIG
 Vue.config.productionTip = false
 
+// APP
 new Vue({
   router,
   render: h => h( App )
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class='has-text-centered'>
+  <div class='has-text-centered' style='margin-top:80px'>
     <img alt='Vue logo' src='../assets/logo.png'>
+    <p><strong>Authentication with Vue and JWT</strong></p>
+    <br>
+    <button class='button is-primary' @click='start'>Start</button>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'home'
+    name: 'home',
+    methods: {
+      start() {
+        
+      }
+    }
   }
 </script>
