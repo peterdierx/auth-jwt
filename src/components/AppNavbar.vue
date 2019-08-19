58f5da83fc07a373f1152f51c2f7044562f2af4f
Peter Dierx <peterdierx@gmail.com>
2019-08-19 12:15:13 +0200

    Signup component

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,27 +1,31 @@
 <template>
   <div class='container' style='margin-top:30px;width:1100px'>
-    <nav class='navbar' role='navigation' aria-label='main navigation'>
-      <!-- BRAND -->
-      <div class='navbar-brand'>
-        <img src='../assets/logo.png' width='60' height='20'>
-      </div>
-      <div class='navbar-item'>
-        <strong>Authentication with Vue and JWT</strong>
-      </div>
-      <!-- AUTH -->
-      <div class='navbar-end'>
+    <div class='box'>
+      <nav class='navbar' role='navigation' aria-label='main navigation'>
+        <!-- BRAND -->
+        <div class='navbar-brand'>
+          <router-link to='/'>
+            <img src='../assets/logo.png' width='60' height='20'>
+          </router-link>
+        </div>
         <div class='navbar-item'>
-          <div class='buttons'>
-            <a class='button is-primary'>
-              <strong>Sign up</strong>
-            </a>
-            <a class='button is-light'>
-              Log in
-            </a>
+          <strong>Authentication with Vue and JWT</strong>
+        </div>
+        <!-- AUTH -->
+        <div class='navbar-end'>
+          <div class='navbar-item'>
+            <div class='buttons'>
+              <router-link to='/signup' class='button is-primary'>
+                <strong>Sign up</strong>
+              </router-link>
+              <a class='button is-light'>
+                Log in
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   </div>
 </template>
 
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue    from 'vue'
 import Router from 'vue-router'
 import Home   from '@/views/Home'
+import Signup from '@/components/Signup'
 
 Vue.use( Router )
 
@@ -8,6 +9,7 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    { path: '/', name: 'home', component: Home }
+    { path: '/',       name: 'home',   component: Home },
+    { path: '/signup', name: 'signup', component: Signup }
   ]
 })
